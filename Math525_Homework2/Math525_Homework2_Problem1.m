9a996181cf094570b5fdef5258f6d63fffcb1e3c
Treece-Burgess <88156797+Treece-Burgess@users.noreply.github.com>
2022-10-16 22:59:45 -0400

    Adding files for homework 2

---
+++ Math525_Homework2/Math525_Homework2_Problem1.m
@@ -0,0 +1,37 @@
+load("normal_normal.mat")
+
+%% Designed Variable Values for M,v, and V
+v = 1;
+M = 0;
+V = 10;
+
+v_prime = (v*V)/(v+V*length(w));
+m_prime = (M*v+V*sum(w))/(v+V*length(w));
+
+%Parameter Space 
+mu = -16:0.01:16;
+
+%Defining posterior and prior
+for i = 1:length(mu)
+prior(1,i) = (1/(sqrt(2*pi*V))) * exp(-((mu(1,i)-M)^2)/(2*V));
+posterior(1,i) = (1/sqrt(2*pi*v_prime))*exp((-1/2)*((mu(1,i)-m_prime)^2)/(v_prime));
+end
+
+%Graphing Prior and Posterior for problem 1 part 3 in homework set 2
+area(mu,prior)
+hold on
+area(mu,posterior)
+legend('Prior','Posterior')
+xlabel('Values of Mu')
+ylabel('Probability Density')
+title('Comparing the Prior and Posterior for a Set of Mu Values')
+hold off
+
+%%Calculating the probability that the prior and posterior will take a
+%%negative value
+prior_prob = @(mu) (1/(sqrt(2*pi*V))) * exp((-(mu-M).^2)/2*V);
+posterior_prob = @(mu) (1/sqrt(2*pi*v_prime))*exp((-1/2)*((mu-m_prime).^2)/(v_prime));
+
+ans_prior_prob = integral(prior_prob,-Inf,0);
+ans_posterior_prob = integral(posterior_prob,-Inf,0);
+
---
+++ Math525_Homework2/Math525_Homework2_Problem2.m
@@ -0,0 +1,41 @@
+load('geo_beta.mat');
+
+%Defining variables 
+a = 1;
+b = 1;
+N = length(w);
+
+
+%Defining pi
+pi_array = 0:0.01:1;
+prior=zeros(1,length(pi_array));
+posterior=zeros(1,length(pi_array));
+
+%Defining posterior and prior
+for i = 1:length(pi_array)
+prior(1,i) = ((pi_array(1,i).^(a-1))*((1-pi_array(1,i)).^(b-1)))/(beta(a,b));
+posterior(1,i) = (pi_array(1,i).^((N+a)-1) * ((1-pi_array(1,i))^((b + sum(w))-1)))/beta(N + a, b+sum(w));
+end
+
+%Graphing Prior and Posterior for problem 2 part 3 in homework set 2
+area(pi_array,prior)
+hold on
+area(pi_array,posterior)
+legend('Prior','Posterior')
+xlabel('Values of pi')
+ylabel('Probability Density')
+title('Comparing the Prior and Posterior for a Set of pi Values')
+hold off
+
+%Calculating the probability that the prior and posterior will take a value
+%of 0.15 or greater for problem 2 part4
+func_prior = @(pi_array) ((pi_array.^(a-1)).*((1-pi_array).^(b-1)))/(beta(a,b));
+func_post = @(pi_array) (pi_array.^((N+a)-1).*((1-pi_array)).^((b + sum(w))-1))/beta(N + a, b+sum(w));
+integral(func_prior,0.15,1)
+integral(func_post,0.15,1)
+
+%Analytically solving for the MAP of the Posterior
+pi_hat = @(pi_array) -(log((pi_array.^((N+a)-1))) + log((1-pi_array).^((b + sum(w))-1)) - log(beta(N + a, b+sum(w))))
+fminsearch(pi_hat,0.1)
+
+((N+a)-1)/((N+a+b+sum(w))-2)
